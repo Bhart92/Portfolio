fdce314074f36dda17e21d67659493e57b15ccde
Brandon Hart <Ihartappraisals@gmail.com>
2019-11-06 12:46:07 -0800

    initial upload

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require('express'),
+      app = express(),
+      bodyParser = require('body-parser'),
+      nodemailer = require("nodemailer"),
+      flash = require('connect-flash'),
+      mongoose = require('mongoose');
+      require("dotenv").config();
+
+let port = process.env.PORT || 3000;
+
+// config
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+app.use(flash());
+app.use(require("express-session")({
+  secret: "hartwebdev",
+  resave: false,
+  saveUninitialized: false
+}));
+
+// config
+app.use(function(req, res, next){
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
+});
+
+app.get('/', function(req, res){
+  res.render('landing');
+});
+app.get('/portfolio', function(req, res){
+  res.render('portfolio');
+});
+app.get('/contact', function(req, res){
+  res.render('contact');
+});
+app.get('/resume', function(req, res){
+  res.render('resume');
+});
+app.post('/', function (req, res) {
+  let mailOpts, smtpTrans;
+  smtpTrans = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: 'hartwebdev92@gmail.com',
+      pass: process.env.GMAILPW
+    }
+  });
+  mailOpts = {
+    from: req.body.name + ' &lt;' + req.body.email + '&gt;',
+    to: 'hartwebdev92@gmail.com',
+    subject: 'New message from hartwebdev.com',
+    text:`${req.body.message}`
+  };
+  smtpTrans.sendMail(mailOpts, function (error, response) {
+    if (error) {
+      req.flash("error", error.responseCode + ", Something went wrong. Please try again!");
+      res.redirect('/');
+    }
+    else {
+      req.flash("success", "Message sent! I'll get back to you as soon as possible.");
+      res.redirect('/');
+    }
+  });
+});
+app.get("*", function(req, res){
+  res.redirect("/");
+});
+
+app.listen(3000, function(){
+  console.log('live');
+});
---
+++ public/js/index.js
@@ -0,0 +1,50 @@
+$(function(){
+  // Error/success message fade
+    $('#positive').delay(2000).fadeOut();
+    $('#negative').delay(2000).fadeOut();
+
+    //lazy load project images
+    $('.projectImageContainer img')
+      .visibility({
+          type       : 'image',
+          transition : 'fade in',
+          duration   : 1500,
+          context: '#innerPortfolioContainer'
+      })
+    ;
+    //
+  //Landing page animation
+    const fadeUp = {
+      transition: 'opacity .75s ease-in, top .8s ease-in',
+      opacity: 1,
+      top: '0px',
+      transitionDelay: '.25s'
+    };
+    const fadeUpDelayed = {
+      transition: 'opacity .75s ease-in, top .8s ease-in',
+      opacity: 1,
+      top: '0px',
+      transitionDelay: '1.15s'
+    };
+    const buttonFade = {
+      transition: 'opacity .75s ease-in',
+      opacity: 1,
+      transitionDelay: '2s'
+    };
+    $('#innerHeaderContainer h1').css(fadeUp);
+    $('#innerHeaderContainer h2').css(fadeUpDelayed);
+    $('#HeaderButtonBox').css(buttonFade);
+  //
+  //mobileNav toggle
+  function navToggle(){
+    $(this).toggleClass('is-active');
+    $('#mobileNav').toggleClass('active');
+    $('body').toggleClass('bodyOverflow');
+  }
+
+  $('.hamburger--squeeze').on('click', function(){
+    $('.hamburger').toggleClass('is-active');
+    $('#mobileNav').toggleClass('active');
+    $('body').toggleClass('bodyOverflow');
+  });
+});
